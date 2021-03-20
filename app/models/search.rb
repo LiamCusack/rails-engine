f9f3012d463909af5994f0a3658bce42c8d39c4d
Liam Cusack <LCusack8108@gmail.com>
2021-03-20 16:44:28 -0600

    stuff

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,11 +1,11 @@
 class Search < ApplicationRecord
   def self.find_one_by_name(params, any_class)
-    any_class.where('name iLIKE ?', "%#{params[:name]}%")
+    any_class.where('name iLIKE ?', "%#{params[:name].downcase}%")
              .first
   end
 
   def self.find_all_by_name(params, any_class)
-    any_class.where('name iLIKE ?', "%#{params[:name]}%")
+    any_class.where('name iLIKE ?', "%#{params[:name].downcase}%")
              .order(:name)
   end
 end
